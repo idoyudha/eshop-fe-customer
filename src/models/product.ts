28c03c16ee424267c7fe80f452a6f2d80fa01a5d
Ido Widya Yudhatama <idowidya.yudhatama@gmail.com>
2024-12-17 16:31:06 +0700

    init: models

---
+++ src/models/product.ts
@@ -0,0 +1,10 @@
+export interface Product {
+    id: string;
+    sku: string;
+    name: string;
+    imageUrl: string;
+    description: string;
+    price: number;
+    quantity: number;
+    category_id: string;
+}
