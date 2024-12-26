952dedb3626df98b07b855db60f7b6654146d3d7
idoyudha <idowidya.yudhatama@gmail.com>
2024-12-26 23:20:49 +0700

    feat: handle logout

--- src/components/nav/auth-nav.tsx
+++ src/components/nav/auth-nav.tsx
@@ -1,7 +1,38 @@
+"use client"
+
+import { useAuth } from "@/context/auth-context";
 import { EshopLink } from "../eshop-link";
-import { buttonVariants } from "../ui/button";
+import { Button, buttonVariants } from "../ui/button";
+import { useState } from "react";
 
 export const AuthNav = () => {
+    const { isAuthenticated, logout } = useAuth();
+    const [isLoading, setIsLoading] = useState(false);
+
+    const handleLogout = async () => {
+        try {
+            setIsLoading(true);
+            await logout();
+        } catch (error) {
+            console.error('Logout error:', error);
+        } finally {
+            setIsLoading(false);
+        }
+    }
+
+    if (isAuthenticated) {
+        return (
+            <Button 
+                variant="outline"
+                size="sm"
+                onClick={handleLogout}
+                disabled={isLoading}
+            >
+                {isLoading ? "Logging out..." : "Logout"}
+            </Button>
+        );
+    }
+
     return (
         <>
             <EshopLink 
