8a82ea9d623c563d7dff0e8bdb1dc3dbff7c3041
idoyudha <idowidya.yudhatama@gmail.com>
2025-01-01 00:06:53 +0700

    feat: order action get

--- src/actions/order-action.ts
+++ src/actions/order-action.ts
@@ -10,28 +10,28 @@ interface OrderResponse {
 }
 
 export async function getOrdersAction(accessToken: string): Promise<Order[]> {
-    try {
-        var orderServiceBaseUrl = getBaseUrl(orderService)
-        const response = await fetch(`${orderServiceBaseUrl}/v1/orders/user`, {
-            method: 'GET',
-            headers: {
-                'Content-Type': 'application/json',
-                'Authorization': `Bearer ${accessToken}`,
-            },
-        });
-
-        if (!response.ok) {
-            throw new Error(`HTTP error! status: ${response.status}`);
-        }
-
-        const orders: OrderResponse = await response.json();
-        if (orders && orders.data) {
-            return structuredClone(orders.data);
-        }
-
-        return [];
-    } catch (error) {
-        console.error('Error get orders:', error);
-        return [];
+    var orderServiceBaseUrl = getBaseUrl(orderService)
+    if (!orderServiceBaseUrl) {
+        orderServiceBaseUrl = process.env.NEXT_PUBLIC_ORDER_SERVICE || "http://localhost:2003"
     }
+
+    const response = await fetch(`${orderServiceBaseUrl}/v1/orders/user`, {
+        method: 'GET',
+        headers: {
+            'Content-Type': 'application/json',
+            'Authorization': `Bearer ${accessToken}`,
+        },
+    });
+
+    if (!response.ok) {
+        throw new Error(`HTTP error! status: ${response.status}`);
+    }
+
+    const orders: OrderResponse = await response.json();
+
+    if (orders && orders.data) {
+        return structuredClone(orders.data);
+    }
+
+    return [];
 }
