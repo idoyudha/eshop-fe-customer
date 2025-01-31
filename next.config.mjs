92d7d6992a7823c6e8f40dea6d814666e144284a
idoyudha <idowidya.yudhatama@gmail.com>
2025-01-31 21:19:39 +0700

    fix: cover all external images links

--- next.config.mjs
+++ next.config.mjs
@@ -5,7 +5,7 @@ const nextConfig = {
         remotePatterns: [
             {
                 protocol: 'https',
-                hostname: process.env.NEXT_PUBLIC_IMAGE_CLOUDFRONT
+                hostname: '**',
             }
         ]
     },
